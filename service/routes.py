64c4efdbe6fb17ff3553d7b9cd3e9dbd692ba2ef
yashasva1 <121627627+yashasva1@users.noreply.github.com>
2025-07-01 19:57:39 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -90,6 +91,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
